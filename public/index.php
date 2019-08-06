ff1355455fd861ada693ec49e4e4992aeacdb7fe
yaroslav <yaroskov@yandex.ru>
2019-08-06 20:11:25 +0300

    firs commit

---
+++ public/index.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="css/style.css">
+    <title>Front Blocks</title>
+</head>
+<body>
+<div class="wrapper">
+    <header class="header">
+        <div class="yar-block yar-bg-purple">
+            <div class="yar-block__content">
+                <h1 class="site-title">Front Blocks</h1>
+            </div>
+        </div>
+    </header>
+
+    <div class="content yar-bg-light-gray">
+        <div class="yar-block">
+            <div class="yar-block__content">
+                <h2 class="page-title">E-market Catalog</h2>
+            </div>
+        </div>
+
+        <div class="yar-block">
+            <div class="yar-block__content yar-bg-white">
+                <div class="yar-filters-panel">
+                    <div class="yar-filters-panel__filter">Filter</div>
+                    <div class="yar-filters-panel__line"></div>
+                    <div class="yar-filters-panel__filter">Filter</div>
+                    <div class="yar-filters-panel__line"></div>
+                    <div class="yar-filters-panel__filter">Filter</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="yar-block">
+            <div class="yar-block__content yar-bg-white">
+                <div class="yar-products">
+                    <div class="yar-products__product">
+                        <div class="yar-products__image"></div>
+                        <div class="yar-products__product-name">Product Name</div>
+                    </div>
+                    <div class="yar-products__product">
+                        <div class="yar-products__image"></div>
+                        <div class="yar-products__product-name">Product Name</div>
+                    </div>
+                    <div class="yar-products__product">
+                        <div class="yar-products__image"></div>
+                        <div class="yar-products__product-name">Product Name</div>
+                    </div>
+                    <div class="yar-products__product">
+                        <div class="yar-products__image"></div>
+                        <div class="yar-products__product-name">Product Name</div>
+                    </div>
+                    <div class="yar-products__product">
+                        <div class="yar-products__image"></div>
+                        <div class="yar-products__product-name">Product Name</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="yar-block">
+            <div class="yar-block__content yar-bg-white">
+                <div class="yar-pagination">
+                    <div class="yar-pagination__page">1</div>
+                    <div class="yar-pagination__page">2</div>
+                    <div class="yar-pagination__page">3</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer yar-bg-purple">
+        <div class="yar-block">
+            <div class="yar-block__content">
+                <div class="info-text">Info Text</div>
+            </div>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
